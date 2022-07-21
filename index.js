73cb537b22c51a4096906e898cc2f180cc24a92c
Paula Belvedere <pau.belve@gmail.com>
2022-07-20 22:43:22 -0300

    se agrega el proyecto

---
+++ index.js
@@ -0,0 +1,73 @@
+
+// Clientes tienda Delfos-cocina
+
+/*  let nombreCliente =  prompt ("Ingrese su nombre")
+  let apellidoCliente =  prompt ("Ingrese su apellido")
+
+  if (nombreCliente =="") { alert ("No ingresaste el nombre de cliente") }
+
+  else alert( " Gracias por elegirnos " + nombreCliente  )
+------------------------------------------------------------------  
+  console.log ("Gracias por elegirnos +nombreCliente+ !");
+
+  alert (`Gracias por elegirnos ${nombreCliente} !`); */
+
+/* DESAFIO CLASE 2 Pedir una nota al usuario y con el uso de condicionales evaluar:
+
+Si la nota es mayor a 10 o menor a 0, devolver que no es una nota válida
+
+En caso contrario, si la nota es igual a 10 calificar con excelente. Si es mayor a 7 calificar con promocionado, de ser mayor o igual a 4 considerar aprobado y menor a 4 como desaprobado.
+
+let nota = 10
+
+if ((nota > "10") || (nota < "0"))
+ {console.log ("no es una nota válida")}
+
+else if (nota == "10") {console.log ("Excelente") } 
+ 
+else if (nota == "7") {console.log ("Promocionado")}
+
+else if (nota >= "4") {console.log ("aprobado")} 
+
+else if (nota < "4") {console.log ("desaprobado")}  */
+
+// Ejercicio clase ciclos
+
+// DESAFIO COMPLEMENTARIO - PRUEBA
+
+ /* let pedido =  prompt("Ingrese la cantidad de budines a pedir")
+    let acumulador = 0
+
+  while (cantidad.toLowerCase() != "no") {
+
+    let cantidadWhile = parseInt (prompt("Desea hacer otro pedido?"))
+     console.log ("La cantidad ingresada es" + cantidadWhile)
+
+     acumulador = acumulador + pedido
+      if (acumulador >=10)
+      console.log ("acumulador + pedido")
+      alert ("Su pedido ha llegado a la cantidad máxima")
+
+      cantidad = ("Desea hacer otro pedido?")} 
+      console.log ("ingrese no para salir") */
+
+      // DESAFIO COMPLEMENTARIO 1
+
+        let pedido = prompt ("Ingrese la cantidad de budines a pedir")
+
+        while (pedido.toLowerCase() != "no") {
+
+        let cantidadWhile = parseInt (prompt("ingrese otra cantidad"))
+        console.log ("la cantidad ingresada es " + cantidadWhile)
+        pedido = prompt ("Desea hacer otro pedido?")
+
+      }
+      
+
+    
+
+
+
+
+
+
