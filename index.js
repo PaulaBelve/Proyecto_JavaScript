c8959e65fcbeaafbea40b21d1fa89a4f5fb2d87f
Paula Belvedere <pau.belve@gmail.com>
2022-07-25 09:16:18 -0300

    desafio-obligario1

--- index.js
+++ index.js
@@ -1,67 +1,68 @@
 
-// Clientes tienda Delfos-cocina
+      // 1er DESAFIO OBLIGATORIO - SIMULADOR CARRITO DE COMPRAS
+     // RESOLVER EL TOTAL DEL PEDIDO DEL CLIENTE 
 
-/*  let nombreCliente =  prompt ("Ingrese su nombre")
-  let apellidoCliente =  prompt ("Ingrese su apellido")
+     function Saludo () {
+      alert ("Bienvenida/o a delfos cocina!")
+     }
 
-  if (nombreCliente =="") { alert ("No ingresaste el nombre de cliente") }
+     // MULTIPLICAR CANTIDAD CON PRECIO, DEPENDIENDO DE LA CANTIDAD, VA A DAR EL TOTAL
 
-  else alert( " Gracias por elegirnos " + nombreCliente  )
-------------------------------------------------------------------  
-  console.log ("Gracias por elegirnos +nombreCliente+ !");
-
-  alert (`Gracias por elegirnos ${nombreCliente} !`); */
-
-/* DESAFIO CLASE 2 Pedir una nota al usuario y con el uso de condicionales evaluar:
-
-Si la nota es mayor a 10 o menor a 0, devolver que no es una nota válida
-
-En caso contrario, si la nota es igual a 10 calificar con excelente. Si es mayor a 7 calificar con promocionado, de ser mayor o igual a 4 considerar aprobado y menor a 4 como desaprobado.
-
-let nota = 10
-
-if ((nota > "10") || (nota < "0"))
- {console.log ("no es una nota válida")}
-
-else if (nota == "10") {console.log ("Excelente") } 
- 
-else if (nota == "7") {console.log ("Promocionado")}
-
-else if (nota >= "4") {console.log ("aprobado")} 
-
-else if (nota < "4") {console.log ("desaprobado")}  */
-
-// Ejercicio clase ciclos
-
-// DESAFIO COMPLEMENTARIO - PRUEBA
-
- /* let pedido =  prompt("Ingrese la cantidad de budines a pedir")
-    let acumulador = 0
-
-  while (cantidad.toLowerCase() != "no") {
-
-    let cantidadWhile = parseInt (prompt("Desea hacer otro pedido?"))
-     console.log ("La cantidad ingresada es" + cantidadWhile)
-
-     acumulador = acumulador + pedido
-      if (acumulador >=10)
-      console.log ("acumulador + pedido")
-      alert ("Su pedido ha llegado a la cantidad máxima")
-
-      cantidad = ("Desea hacer otro pedido?")} 
-      console.log ("ingrese no para salir") */
-
-      // DESAFIO COMPLEMENTARIO 1
-
-        let pedido = prompt ("Ingrese la cantidad de budines a pedir")
-
-        while (pedido.toLowerCase() != "no") {
-
-        let cantidadWhile = parseInt (prompt("ingrese otra cantidad"))
-        console.log ("la cantidad ingresada es " + cantidadWhile)
-        pedido = prompt ("Desea hacer otro pedido?")
-
+     function calcularPedido () {
+       
+      let cantidad = parseInt(prompt("Ingrese la cantidad de budines a pedir"))
+      let precio = 1000
+      
+      let resultado = cantidad * precio ;
+      return resultado
+      
       }
+
+    function ValidarEnvio (total) {
+   
+    if (total <= 2000) {alert ("Debe abonar el recargo del envio")} 
+    
+    else if (total >=5000) {alert ("Su pedido es mayor a 5000, el envio es sin cargo")} }
+
+    // INVOCACIÓN DE LAS FUNCTION
+
+    Saludo ()
+    
+    let total = calcularPedido ()
+
+    ValidarEnvio (total)
+
+    alert ("Gracias por su compra! El total del pedido es " + total)
+     
+    
+    
+    
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+     
+
+
+
       
 
     
