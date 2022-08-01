b3e37701f9b113d5efb8e83d90f96d31ad50c127
Paula Belvedere <pau.belve@gmail.com>
2022-08-01 03:29:00 -0300

    desafio-complementario-arrays

--- index.js
+++ index.js
@@ -1,38 +1,139 @@
 
-      // 1er DESAFIO OBLIGATORIO - SIMULADOR CARRITO DE COMPRAS
-     // RESOLVER EL TOTAL DEL PEDIDO DEL CLIENTE 
 
-     function Saludo () {
-      alert ("Bienvenida/o a delfos cocina!")
-     }
+    // OBJETOS
 
-     // MULTIPLICAR CANTIDAD CON PRECIO, DEPENDIENDO DE LA CANTIDAD, VA A DAR EL TOTAL
+  class budines {  
+    
+    constructor(id, nombre, sabor, cantidad, precio) {
+    
+    this.id = id,
+    this.nombre = nombre,
+    this.sabor = sabor,
+    this.cantidad = cantidad,
+    this.precio = precio
+  
+  }
 
-     function calcularPedido () {
+  mostrarBudines() { 
+  
+    console.log(`El nombre es ${this.nombre}, su sabor ${this.sabor}, el precio es de ${this.precio}, la id es ${this.id}` )} 
+  
+  }
+
+ const budines1 = new budines (1,"venus", "cacaoybanana", 2, 1600 )
+ 
+ const budines2 = new budines (1,"gea", "coco", 2, 1200 )
+ 
+ const budines3 = new budines (3,"poseidon", "frutosrojos", 2, 2000 )
+ 
+ const budines4 = new budines (4,"apollo", "naranja", 2, 1500)
+ 
+// Array de budines
+
+ const carrito = [budines1, budines2, budines3, budines4]
+ console.log (carrito)
+
+ // Funciones
+
+ // Bienvenida del sitio
+
+ // Función que le permita al cliente agregar productos al carrito
+
+ function NuevoPedido () {
+
+  alert("Podrá elegir entre estas opciones: venus, gea, poseidon y apollo")
+
+  let nombreIngresado = prompt("Ingrese el nombre del budin que desea agregar al carrito")
+  
+  let saborIngresado = prompt("Ingrese el sabor")
+  
+  let cantidadIngresada = parseInt(prompt("Ingrese la cantidad que desea pedir"))
+  
+  let pedidoRealizado = new budines (carrito.length+1,nombreIngresado,saborIngresado,cantidadIngresada)
+  console.log (pedidoRealizado)
+  carrito.push(pedidoRealizado)
+}
+
+// Función que permita mostrar el catálogo en consola
+
+function Catalogo () {
+
+  alert ("Podrá ver nuestras opciones en consola")
+
+  for (let budines of carrito) 
+  {console.log (budines)}
+
+}
+
+ // Función menu de opciones
+
+function Menu () {
+
+ let opciones = parseInt(prompt(`Bienvenida/o a delfos cocina! Ingrese la opción que desee realizar:
+                      
+                       1 - Mirar la lista de nuestros productos
+                       2 - Agregar productos al carrito
+                       3 - Eliminar del carrito
+                       0 - Finalizar`))
+
+                       menuDetallado(opciones)
+
+                       } 
+
+//Función del menu
+function menuDetallado (opcionSeleccionada){
+  switch(opcionSeleccionada){
+      case 0:
+          Finalizar = true
+          alert(`Gracias por elegirnos!`)
+      break    
+      case 1:
+          Catalogo()
+
+      break 
+       case 2: 
+          NuevoPedido()
+      break 
+       case 3: 
+          //eliminarLibro()
        
-      let cantidad = parseInt(prompt("Ingrese la cantidad de budines a pedir"))
-      let precio = 1000
+      break 
+       default: 
+      alert(`Ingrese una opción correcta`)
+  }
+}
+
+//CÓDIGO:
+let Finalizar 
+while(Finalizar != true){
+    Menu()
+  
+}
+
+// Invocación funciones
+  
+
+
+
+
+
+
+
+
+
+  
+  
+  
+ 
+
+     
       
-      let resultado = cantidad * precio ;
-      return resultado
-      
-      }
+ 
 
-    function ValidarEnvio (total) {
-   
-    if (total <= 2000) {alert ("Debe abonar el recargo del envio")} 
-    
-    else if (total >=5000) {alert ("Su pedido es mayor a 5000, el envio es sin cargo")} }
+ 
 
-    // INVOCACIÓN DE LAS FUNCTION
 
-    Saludo ()
-    
-    let total = calcularPedido ()
 
-    ValidarEnvio (total)
-
-    alert ("Gracias por su compra! El total del pedido es " + total)
      
     
     
