76ccb2ddce14967fce5ba896a8ca26b2f4fbb3ca
Paula Belvedere <pau.belve@gmail.com>
2022-08-03 10:09:34 -0300

    sumando carrito de compras

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+// DESAFIO COMPLEMENTARIO - ARRAYS
 
     // OBJETOS
 
@@ -20,52 +20,58 @@
   
   }
 
- const budines1 = new budines (1,"venus", "cacaoybanana", 2, 1600 )
+ const budines1 = new budines (1,"venus", "cacaoybanana", "" , 1600 )
  
- const budines2 = new budines (1,"gea", "coco", 2, 1200 )
+ const budines2 = new budines (2,"gea", "coco", "", 1200 )
  
- const budines3 = new budines (3,"poseidon", "frutosrojos", 2, 2000 )
+ const budines3 = new budines (3,"poseidon", "frutosrojos", "", 2000 )
  
- const budines4 = new budines (4,"apollo", "naranja", 2, 1500)
+ const budines4 = new budines (4,"apollo", "naranja", "", 1500)
  
 // Array de budines
 
- const carrito = [budines1, budines2, budines3, budines4]
+ const productos = [budines1, budines2, budines3, budines4];
+ console.log (productos)
+
+ const carrito = [];
  console.log (carrito)
 
  // Funciones
 
- // Bienvenida del sitio
-
  // Función que le permita al cliente agregar productos al carrito
 
- function NuevoPedido () {
+  function NuevoPedido () {
+  let elegirProductos = prompt(`
+            Eliga el número correspondiente del budin que desea comprar
+           
+            1. Nombre: ${productos[0].nombre}
+            Precio: $${productos[0].precio}
+            2. Nombre: ${productos[1].nombre}
+            Precio: $${productos[1].precio}
+            3. Nombre: ${productos[2].nombre}
+            Precio: $${productos[2].precio}
+            4. Nombre: ${productos[3].nombre}
+            Precio: $${productos[3].precio}
+        `)
 
-  alert("Podrá elegir entre estas opciones: venus, gea, poseidon y apollo")
+        console.log (NuevoPedido)
 
-  let nombreIngresado = prompt("Ingrese el nombre del budin que desea agregar al carrito")
-  
-  let saborIngresado = prompt("Ingrese el sabor")
-  
-  let cantidadIngresada = parseInt(prompt("Ingrese la cantidad que desea pedir"))
-  
-  let pedidoRealizado = new budines (carrito.length+1,nombreIngresado,saborIngresado,cantidadIngresada)
-  console.log (pedidoRealizado)
-  carrito.push(pedidoRealizado)
+        menuBudines(elegirProductos)
+ 
 }
 
-// Función que permita mostrar el catálogo en consola
+// Function que permita mostrar el catálogo en consola
 
 function Catalogo () {
 
   alert ("Podrá ver nuestras opciones en consola")
 
-  for (let budines of carrito) 
+  for (let budines of productos) 
   {console.log (budines)}
 
 }
 
- // Función menu de opciones
+ // Function menu principal de opciones
 
 function Menu () {
 
@@ -80,7 +86,8 @@ function Menu () {
 
                        } 
 
-//Función del menu
+//Funtion elegir la opción del menu principal
+
 function menuDetallado (opcionSeleccionada){
   switch(opcionSeleccionada){
       case 0:
@@ -93,6 +100,7 @@ function menuDetallado (opcionSeleccionada){
       break 
        case 2: 
           NuevoPedido()
+
       break 
        case 3: 
           //eliminarLibro()
@@ -104,13 +112,122 @@ function menuDetallado (opcionSeleccionada){
 }
 
 //CÓDIGO:
+
 let Finalizar 
 while(Finalizar != true){
-    Menu()
+    
+  Menu()
   
 }
 
 // Invocación funciones
+
+// Function que suma nombre - precio - cantidad
+
+function menuBudines (elegirProductos) {
+
+
+switch (elegirProductos) {
+      
+      case "1": (prompt(`ingrese la cantidad que desea pedir:
+        ${productos[0].cantidad}`))
+      carrito.push(productos[0])
+      console.log(carrito)
+      resumenDeCompra("1")
+      break 
+       
+      case "2": (prompt(`ingrese la cantidad que desea pedir:
+      ${productos[1].cantidad}`))
+       carrito.push(productos[1])
+       console.log(carrito)
+       resumenDeCompra("2")
+       break 
+       
+       case "3": prompt(`ingrese la cantidad que desea pedir: 
+       ${productos[2].cantidad}`)
+       carrito.push(productos[2])
+       console.log(carrito)
+       resumenDeCompra("3")
+       break 
+      
+       case "4": (prompt(`ingrese la cantidad que desea pedir:
+        ${productos[3].cantidad}`))
+       carrito.push(productos[3])
+       console.log(carrito)
+       resumenDeCompra("4")
+      
+       break 
+       default: 
+      alert(`Ingrese una opción correcta`)
+       
+    } }
+
+// Function opcion para seguir comprando o finalizar la compra
+
+function resumenDeCompra (elegirProductos) {
+
+  let seguirComprando = ""
+
+  switch (elegirProductos) {
+      case "1" : 
+
+      seguirComprando = prompt("¿Desea hacer otra compra? si/no").toLowerCase();
+
+                if (seguirComprando === "si") {
+                    NuevoPedido()
+                } else {
+                   alert ("Gracias por tu compra!");
+                }
+                break;
+                case "2" : 
+
+      seguirComprando = prompt("¿Desea hacer otra compra? si/no").toLowerCase();
+
+                if (seguirComprando === "si") {
+                    NuevoPedido()
+                } else {
+                   alert ("Gracias por tu compra!");
+                }
+                break;
+                case "3" : 
+
+      seguirComprando = prompt("¿Desea hacer otra compra? si/no").toLowerCase();
+
+                if (seguirComprando === "si") {
+                    NuevoPedido()
+                } else {
+                   alert ("Gracias por tu compra!");
+                }
+                break;
+                case "4" : 
+
+      seguirComprando = prompt("¿Desea hacer otra compra? si/no").toLowerCase();
+
+                if (seguirComprando === "si") {
+                    NuevoPedido()
+                } else {
+                   alert ("Gracias por tu compra!");
+                }
+                break; 
+       default: 
+      alert(`Ingrese una opción correcta`)
+      resumenDeCompra (elegirProductos)
+       break    } }
+
+       carrito.forEach((productos)=> {
+        console.log(`${productos.nombre} - $${productos.precio}`)
+    }) 
+    
+    alert(`El total de compra es: $${CompraTotal()}`)
+  
+  function CompraTotal(){
+
+           let total = carrito.reduce((total, budines) => total + budines.precio, 0);
+      return total;
+  }
+
+
+
   
 
 
@@ -154,19 +271,6 @@ while(Finalizar != true){
 
  
 
-
-
-
-
-
-
-     
-
-
-
-      
-
-    
 
 
 
