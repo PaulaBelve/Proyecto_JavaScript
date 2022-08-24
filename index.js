91dede086b1f503dfaa52b7f03dd5316394749af
Paula Belvedere <pau.belve@gmail.com>
2022-08-24 20:40:43 -0300

    aplicando operador ternario - sweet alert

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+// Bienvenida a la tienda
+
+Swal.fire ({
+
+  title: `Bienvenido/a`,
+  text: `Tienda oficial de delfos cocina` ,
+  imageUrl: `img/delfos sombra.png`,
+  imageWidth: 300,
+  imageHeigth: 100,
+  timer: 3000,
+  imageAlt:  `Custom image` ,
+  showConfirmButton: false ,
+})
+
+
 //SIMULADOR CARRITO
 
 // OBJETO BUDINES
@@ -43,6 +58,12 @@ let parrafoEnvio = document.getElementById('parrafoEnvio');
 let parrafoTotal = document.getElementById('totalPedido');
 let acumulador=0;
 let divBudines = document.getElementById('cards-productos');
+// Get the modal
+let modal = document.getElementById('myModal');
+// Get the button that opens the modal
+let btn = document.getElementById('myBtn');
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName('close')[0];
 
 // Array productos que se imprima en el local Storage.
 
@@ -117,6 +138,15 @@ function agregarCarrito(budin) {
   arrayCarrito.push(budin);
   console.log(arrayCarrito);
   localStorage.setItem('Carrito', JSON.stringify(arrayCarrito));
+
+  Swal.fire({
+    title: "Su producto ha sido seleccionado",
+    text: `El budin ${budin.nombre} ha sido agregado al carrito`,
+    icon: "success",
+    timer: 2000,
+  //  showConfirmButton: false,
+    confirmButtonText:"OK",
+})
 }
 
 // Evento botonCarrito - llama a la función de productosAlcarrito
@@ -164,44 +194,28 @@ console.log(acumulador)
  
   //if acumulador = 0 o !=
 
- acumulador > 0 ? `El total de su compra es de: ${acumulador}` :  `<p>No hay productos en el carrito</p>` 
+  const result = acumulador >= 0 ? `El total de su compra es de: ${acumulador}` :  `No hay productos en el carrito`
 
+  return result
+ 
 } ;
 
- 
-
- //;
-/*  if (acumulador == 0) {
-    return `<p>No hay productos en el carrito</p>`;
-  } else {
-    return `El total de su compra es de: ${acumulador}`;
-  }
-} */ 
 
 // Función que calcule el envio - envio sin costo - $200 de envio - (IF - ELSE)
 
 function ValidarEnvio(acu) {
 
-  const data = acu >= 3000 ? `<p>Envio sin cargo!</p>` : `<p>Debe abonar $200 de envío</p>` ;
+  const data = acu >= 3000 ? `Envio sin cargo!` : `Debe abonar $200 de envío` ;
 
-console.log(data)  } 
- 
- /* if (acu <= 1500) {
-    return `<p>Debe abonar $200 de envío</p>`;
-  } else if (acu >= 3000) {
-    return `<p>Envio sin cargo!</p>`;
-  } */
+  return data
 
-// Get the modal
-let modal = document.getElementById('myModal');
+  } 
 
-// Get the button that opens the modal
-let btn = document.getElementById('myBtn');
+ // Invocar función donde se agregan los productos al carrito
 
-// Get the <span> element that closes the modal
-let span = document.getElementsByClassName('close')[0];
+ // When the user clicks on the button, open the modal
 
-// When the user clicks on the button, open the modal
+function productosModal () {
 
 botonCarrito.onclick = function () {
 
@@ -215,24 +229,27 @@ botonCarrito.onclick = function () {
   
                           <div class="boxBudines__info">
                           <h2 class="boxBudines__title">"${item.nombre}"</h2>
-                          <p class="boxBudines__precio"> Precio: "$${item.precio}"</p>       
+                          <p class="boxBudines__precio"> Precio: "$${item.precio}"</p> 
+
         </div>  
         </article>   
     </div>`
 
-  )
+)
 
-  // CORREGIR - CADA VEZ QUE TOCO EL CARRITO ME SUMA MAS DEL BUDIN SELECCIONADO
-  
   console.log(acumulador) 
+
   const parrafos=`
-  <p id="parrafoTotal">${compraTotal(...arrayCarrito)}</p>
+  <p id="parrafoTotal">$${compraTotal(...arrayCarrito)}</p>
   <p id="parrafoEnvio">${ValidarEnvio(acumulador)}</p>
   `
   const aux=carts.join("") +parrafos
   document.getElementById('modal-body').innerHTML = aux;
   modal.style.display = 'block'; }; 
 
+  }
+
+productosModal ()
 
 // When the user clicks on <span> (x), close the modal
 
@@ -252,25 +269,27 @@ window.onclick = function (event) {
 
 //Desestructurando
 
-let [a, ,b , c ,d] = productos
+/* let [a, ,b , c ,d] = productos
 a = "budin nuevo"
 b = "budin limon"
 console.log(a)
 console.log(b)
 console.log(c)
 console.log(d)
-console.log(productos)
+console.log(productos) */
 
 // Nulish
 
-let budinBuscado = productos.find(budin => budin.nombre == "Venus") ?? "No tenemos ese budin en stock"
-console.log(budinBuscado)
+/* let budinBuscado = productos.find(budin => budin.nombre == "Venus") ?? "No tenemos ese budin en stock"
+console.log(budinBuscado) */
 
 // Operador and
 
-let numero = 10 ;
+/* let numero = 10 ;
 
-numero < 11 && console.log("es menor que 10")
+numero < 11 && console.log("es menor que 10") */
+
+
 
 
 
